776b0fa40b5c808bf06b525cd840d4c207165334
Kang-dot <jgbu97@gmail.com>
2025-01-20 14:15:01 +0900

    [APIS-1013] edit test batch file
    - edit ado.net driver project file
    - edit test batch file
      - change test program(vstest)

--- test.bat
+++ test.bat
@@ -1,5 +1,8 @@
+set BATCH_HOME=%~dp0
+set VSTestTool=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
+
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
-cd Code\Test\QATest\ADOTest
-devenv ADOTest.csproj /rebuild "Release|Any CPU" 
-cd bin\Release
-MSTest/testcontainer:ADOTest.dll
+cd Code\
+devenv CUBRID.Data.sln /Project ADOTest /Rebuild "Release|x64" 
+cd Test\QATest\ADOTest\bin\Release
+call "%VSTestTool%" ADOTest.dll /Platform:x64 /Settings:%BATCH_HOME%Code\TestSettings1.testsettings
